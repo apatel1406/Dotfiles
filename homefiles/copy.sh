b939f62111a10bbe5c7ffd32e57602c799755a35
Akshar Patel <patelakshar1406@gmail.com>
2026-04-24 23:02:47 -0400

    Added comments for better code understanding
    Added comments to clarify the purpose of the script.

--- homefiles/copy.sh
+++ homefiles/copy.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
-#
-
+# Path to the source .bash_aliases file in my dotfiles directory
 ALIAS="$HOME/dotfiles/homefiles/.bash_aliases"
 MOVE="$HOME/.bash_aliases"
 
+# Copy the aliases file from the dotfiles location to the home directory
 cp "$ALIAS" "$MOVE"
 echo "Copied .bach_aliases to home directory"
 
+# Provide instructions to reload the shell environment
 echo "Reload your environment by running 'source ~/.bash_aliases', or you can log out and log back in."
