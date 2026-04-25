2cef4b323ec1da86066e5d4de0b2d09e93f74725
Akshar Patel <patelakshar1406@gmail.com>
2026-04-24 22:56:12 -0400

    Update copy.sh to manage .vimrc and Vundle installation
    Copy .vimrc from dotfiles and install Vundle if missing.

--- vim-config/copy.sh
+++ vim-config/copy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
+cp /home/g_sigler/dotfiles/vim-config/.vimrc ~/.vimrc
 
-cp vim-config/.vimrc ~/.vimrc
+if [ ! -d "/home/patel/.vim/bundle/Vundle.vim" ]; then
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
+
+vim +PluginInstall +qall
